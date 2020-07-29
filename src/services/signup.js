87ffe2921e2204df217a340ab487232fac883afa
Zhou Jian <zhoujian0998@gmail.com>
2020-07-29 17:16:05 +0200

    added signup service

--- src/services/signup.js
+++ src/services/signup.js
@@ -6,58 +6,69 @@ module.exports = (pool, grpcStatus, email, password, username, iban) =>
           code: grpcStatus.UNAVAILABLE,
           message: err.sqlMessage,
         });
-
-      connection.beginTransaction((err) => {
-        if (err)
-          return connection.rollback(() => {
+      startTransaction(connection, grpcStatus)
+        .then(() => addUser(connection, grpcStatus, email, password, username))
+        .then(({ user_id }) => addIban(connection, grpcStatus, user_id, iban))
+        .then(({ user_id }) => commit(connection, grpcStatus, user_id))
+        .then(({ user_id }) => {
+          connection.release();
+          res({ id: user_id });
+        })
+        .catch((err) => {
+          connection.rollback(() => {
             connection.release();
-            rej({
-              code: grpcStatus.ABORTED,
-              message: err.sqlMessage,
-            });
+            rej(err);
           });
+        });
+    });
+  });
 
-        const addUserQuery = `INSERT INTO ${process.env.DB_DATABASE}.user (email, password, username) VALUES (?, ?, ?);`;
-        connection.query(
-          addUserQuery,
-          [email, password, username],
-          (err, results) => {
-            if (err)
-              return connection.rollback(() => {
-                connection.release();
-                rej({
-                  code: grpcStatus.ABORTED,
-                  message: err.sqlMessage,
-                });
-              });
+const startTransaction = (connection, grpcStatus) =>
+  new Promise((res, rej) => {
+    connection.beginTransaction((err) => {
+      if (err)
+        return rej({
+          code: grpcStatus.ABORTED,
+          message: err.sqlMessage,
+        });
+      res({});
+    });
+  });
 
-            const { insertId } = results;
-            const addIbanQuery = `INSERT INTO ${process.env.DB_DATABASE}.bank_account (user_id, iban) VALUES (?, ?);`;
-            connection.query(addIbanQuery, [insertId, iban], (err, results) => {
-              if (err)
-                return connection.rollback(() => {
-                  connection.release();
-                  rej({
-                    code: grpcStatus.ABORTED,
-                    message: err.sqlMessage,
-                  });
-                });
+const addUser = (connection, grpcStatus, email, password, username) =>
+  new Promise((res, rej) => {
+    const query = `INSERT INTO ${process.env.DB_DATABASE}.user (email, password, username) VALUES (?, ?, ?);`;
+    connection.query(query, [email, password, username], (err, results) => {
+      if (err)
+        return rej({
+          code: grpcStatus.ABORTED,
+          message: err.sqlMessage,
+        });
+      res({ user_id: results.insertId });
+    });
+  });
 
-              connection.commit((err) => {
-                if (err)
-                  return connection.rollback(() => {
-                    connection.release();
-                    rej({
-                      code: grpcStatus.ABORTED,
-                      message: err.sqlMessage,
-                    });
-                  });
-                connection.release();
-                return res({ message: "Success operation", code: 200 });
-              });
-            });
-          }
-        );
-      });
+const addIban = (connection, grpcStatus, user_id, iban) =>
+  new Promise((res, rej) => {
+    const query = `INSERT INTO ${process.env.DB_DATABASE}.bank_account (user_id, iban) VALUES (?, ?);`;
+    connection.query(query, [user_id, iban], (err, results) => {
+      if (err)
+        return rej({
+          code: grpcStatus.ABORTED,
+          message: err.sqlMessage,
+        });
+      res({ user_id: user_id });
+    });
+  });
+
+const commit = (connection, grpcStatus, user_id) =>
+  new Promise((res, rej) => {
+    connection.commit((err) => {
+      if (err)
+        return rej({
+          code: grpcStatus.ABORTED,
+          message: err.sqlMessage,
+        });
+      res({ user_id: user_id });
     });
   });
