388afe50bc7e36f6def8922658cec783088a6a3d
Zhou Jian <zhoujian0998@gmail.com>
2020-07-30 13:59:02 +0200

    added test on deposit via token on metadata

--- test/index.js
+++ test/index.js
@@ -45,3 +45,26 @@ const user_proto = grpc.loadPackageDefinition(packageDefinition).user;
     }
   );
 })();
+(function main() {
+  const client = new user_proto.User(
+    "localhost:9001",
+    grpc.credentials.createInsecure()
+  );
+  const metadata = new grpc.Metadata();
+  metadata.add(
+    "token",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaWF0IjoxNTk2MTA3MTUwLCJleHAiOjE2Mjc2NDMxNTB9.ZCxvsJQngIwxi-9CW4KZ2fEcLN-cMqF67LEfKeSCKGQ"
+  );
+  client.deposit(
+    {
+      user_id: 1,
+      currency: "EUR",
+      amount: 10,
+    },
+    metadata,
+    (err, response) => {
+      if (err) return console.log(err);
+      console.log(response);
+    }
+  );
+})();
