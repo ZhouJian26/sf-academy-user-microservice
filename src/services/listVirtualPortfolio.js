aa76f99c34a3e1e37a1f721cba32a8c9563143f9
Zhou Jian <zhoujian0998@gmail.com>
2020-08-01 14:22:34 +0200

    list virtual portfolio parameter fic

--- src/services/listVirtualPortfolio.js
+++ src/services/listVirtualPortfolio.js
@@ -1,11 +1,12 @@
 module.exports = (pool, grpcStatus, user_id) =>
   new Promise((res, rej) => {
+    console.log(user_id);
     getVirtualPortfolios(pool, grpcStatus, user_id)
       .then((results) => res(results))
       .catch((e) => rej(e));
   });
 
-const getVirtualPortfolios = (pool, grpcStatus, amount, user_id, currency) =>
+const getVirtualPortfolios = (pool, grpcStatus, user_id) =>
   new Promise((res, rej) => {
     const query = `SELECT currency, balance, date FROM ${process.env.DB_DATABASE}.virtual_portfolio WHERE user_id = ?;`;
     pool.query(query, [user_id], (err, results) => {
