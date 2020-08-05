0db3f6d6502f0824c50f20f9ed0130b67b77be74
Zhou Jian <zhoujian0998@gmail.com>
2020-08-05 18:05:26 +0200

    data filter fix

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-"use strict";
-const PROTO_PATH = __dirname + "/../sf-academy-proto/src/user.proto";
+'use strict';
+const PROTO_PATH = __dirname + '/../sf-academy-proto/src/user.proto';
 
-const grpc = require("grpc");
-const protoLoader = require("@grpc/proto-loader");
-const mysql = require("mysql");
-const crypto = require("crypto");
-const jwt = require("jsonwebtoken");
+const grpc = require('grpc');
+const protoLoader = require('@grpc/proto-loader');
+const mysql = require('mysql');
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
 
-const dotenv = require("dotenv").config();
+const dotenv = require('dotenv').config();
 
 const packageDefinition = protoLoader.loadSync(PROTO_PATH, {
   keepCase: true,
@@ -27,134 +27,119 @@ const pool = mysql.createPool({
   connectionLimit: 10,
 });
 
-const generateJWT = ({ id }) =>
-  jwt.sign({ id: id }, process.env.JWT_KEY, { expiresIn: 60 * 60 * 24 * 365 });
+const generateJWT = ({id}) =>
+    jwt.sign({id: id}, process.env.JWT_KEY, {expiresIn: 60 * 60 * 24 * 365});
 
-const auth = (token) =>
-  new Promise((res, rej) => {
-    jwt.verify(token[0], process.env.JWT_KEY, (err, decoded) => {
-      if (err)
-        return rej({
-          code: grpc.status.PERMISSION_DENIED,
-          message: err.name + ": " + err.message,
-        });
-      res({ decoded: decoded });
-    });
+const auth = (token) => new Promise((res, rej) => {
+  jwt.verify(token[0], process.env.JWT_KEY, (err, decoded) => {
+    if (err)
+      return rej({
+        code: grpc.status.PERMISSION_DENIED,
+        message: err.name + ': ' + err.message,
+      });
+    res({decoded: decoded});
   });
+});
 
-const exchangeMicroservice = require("./other/exchange");
+const exchangeMicroservice = require('./other/exchange');
 
-const signupService = require("./services/signup");
-const loginService = require("./services/login");
-const depositService = require("./services/deposit");
-const withdrawService = require("./services/withdraw");
-const buyService = require("./services/buy");
-const listTransactionsService = require("./services/listTransactions");
-const listVirtualPortfoliosService = require("./services/listVirtualPortfolio");
+const signupService = require('./services/signup');
+const loginService = require('./services/login');
+const depositService = require('./services/deposit');
+const withdrawService = require('./services/withdraw');
+const buyService = require('./services/buy');
+const listTransactionsService = require('./services/listTransactions');
+const listVirtualPortfoliosService = require('./services/listVirtualPortfolio');
 
 const services = {
   signup: (call, callback) => {
-    const { email, password, username, iban } = call.request;
-    if (
-      email == undefined ||
-      password == undefined ||
-      username == undefined ||
-      iban == undefined ||
-      email == "" ||
-      password == "" ||
-      iban == "" ||
-      username == ""
-    )
+    const {email, password, username, iban} = call.request;
+    if (email == undefined || password == undefined || username == undefined ||
+        iban == undefined || email == '' || password == '' || iban == '' ||
+        username == '')
       return callback({
         code: grpc.status.INVALID_ARGUMENT,
-        message: "Missing values",
+        message: 'Missing values',
       });
     signupService(
-      pool,
-      grpc.status,
-      email,
-      crypto.createHash("sha256").update(password).digest("hex"),
-      username,
-      iban
-    )
-      .then((data) => {
-        console.log(data);
-        callback(null, { token: generateJWT(data) });
-      })
-      .catch((e) => callback(e));
+        pool, grpc.status, email,
+        crypto.createHash('sha256').update(password).digest('hex'), username,
+        iban)
+        .then((data) => {
+          console.log(data);
+          callback(null, {token: generateJWT(data)});
+        })
+        .catch((e) => callback(e));
   },
   login: (call, callback) => {
-    const { email, password } = call.request;
+    const {email, password} = call.request;
     if (email == undefined || password == undefined)
       return callback({
         code: grpc.status.INVALID_ARGUMENT,
-        message: "Missing values",
+        message: 'Missing values',
       });
     loginService(
-      pool,
-      grpc.status,
-      email,
-      crypto.createHash("sha256").update(password).digest("hex")
-    )
-      .then((data) => callback(null, { token: generateJWT(data) }))
-      .catch((e) => callback(e));
+        pool, grpc.status, email,
+        crypto.createHash('sha256').update(password).digest('hex'))
+        .then((data) => callback(null, {token: generateJWT(data)}))
+        .catch((e) => callback(e));
   },
   deposit: (call, callback) => {
-    auth(call.metadata.get("token"))
-      .then(({ decoded }) =>
-        depositService(pool, grpc.status, decoded.id, call.request)
-      )
-      .then(() => callback(null, {}))
-      .catch((e) => callback(e));
+    auth(call.metadata.get('token'))
+        .then(
+            ({decoded}) =>
+                depositService(pool, grpc.status, decoded.id, call.request))
+        .then(() => callback(null, {}))
+        .catch((e) => callback(e));
   },
   withdraw: (call, callback) => {
-    auth(call.metadata.get("token"))
-      .then(({ decoded }) =>
-        withdrawService(pool, grpc.status, decoded.id, call.request)
-      )
-      .then(() => callback(null, {}))
-      .catch((e) => callback(e));
+    auth(call.metadata.get('token'))
+        .then(
+            ({decoded}) =>
+                withdrawService(pool, grpc.status, decoded.id, call.request))
+        .then(() => callback(null, {}))
+        .catch((e) => callback(e));
   },
   buy: (call, callback) => {
-    auth(call.metadata.get("token")) //get rate from exchange micro
-      .then(({ decoded }) => {
-        const { srcCurrency, destCurrency, amount } = call.request;
-        return new exchangeMicroservice(process.env.EXCHANGE_MICROSERVICE_URL)
-          .getExchange(amount, srcCurrency, destCurrency, grpc.status)
-          .then(({ value, rate }) => ({
-            user_id: decoded.id,
-            rate: rate,
-            value: value,
-            srcCurrency: srcCurrency,
-            destCurrency: destCurrency,
-            amount: amount,
-          }));
-      })
-      .then((data) => buyService(pool, grpc.status, data))
-      .then((data) => callback(null, data))
-      .catch((e) => callback(e));
+    auth(call.metadata.get('token'))  // get rate from exchange micro
+        .then(({decoded}) => {
+          const {srcCurrency, destCurrency, amount} = call.request;
+          return new exchangeMicroservice('exchange_microservice:9000')
+              .getExchange(amount, srcCurrency, destCurrency, grpc.status)
+              .then(({value, rate}) => ({
+                      user_id: decoded.id,
+                      rate: rate,
+                      value: value,
+                      srcCurrency: srcCurrency,
+                      destCurrency: destCurrency,
+                      amount: amount,
+                    }));
+        })
+        .then((data) => buyService(pool, grpc.status, data))
+        .then((data) => callback(null, data))
+        .catch((e) => callback(e));
   },
   listTransactions: (call, callback) => {
-    auth(call.metadata.get("token"))
-      .then(({ decoded }) =>
-        listTransactionsService(pool, grpc.status, decoded.id, call.request)
-      )
-      .then((results) => callback(null, { transactions: results }))
-      .catch((e) => callback(e));
+    auth(call.metadata.get('token'))
+        .then(
+            ({decoded}) => listTransactionsService(
+                pool, grpc.status, decoded.id, call.request))
+        .then((results) => callback(null, {transactions: results}))
+        .catch((e) => callback(e));
   },
   listVirtualPortfolios: (call, callback) => {
-    auth(call.metadata.get("token"))
-      .then(({ decoded }) =>
-        listVirtualPortfoliosService(pool, grpc.status, decoded.id)
-      )
-      .then((results) => callback(null, { listVirtualPortfolio: results }))
-      .catch((e) => callback(e));
+    auth(call.metadata.get('token'))
+        .then(
+            ({decoded}) =>
+                listVirtualPortfoliosService(pool, grpc.status, decoded.id))
+        .then((results) => callback(null, {listVirtualPortfolio: results}))
+        .catch((e) => callback(e));
   },
 };
 
 (function main() {
   const server = new grpc.Server();
   server.addService(user_proto.User.service, services);
-  server.bind("0.0.0.0:9001", grpc.ServerCredentials.createInsecure());
+  server.bind('0.0.0.0:9001', grpc.ServerCredentials.createInsecure());
   server.start();
 })();
--- src/services/listTransactions.js
+++ src/services/listTransactions.js
@@ -5,16 +5,35 @@ const sqlMapping = {
   destCurrency: "VP2.currency",
 };
 
-const sqlOpMapping = (keys, target) => {
+const sqlOpMapping = (target) => {
   switch (target) {
     case "currency":
       return "=";
     case "startDate":
-      return keys.contains("endDate") ? "<=" : "=";
+      return ">=";
     case "endDate":
-      return keys.contains("startDate") ? ">=" : "=";
+      return "<=";
     default:
       return "=";
+  }
+};
+
+const parseInput = (key, value) => {
+  switch (value) {
+    case "startDate":
+      return new Date(query[key])
+        .toISOString()
+        .split("T")
+        .join(" ")
+        .split(":")[0];
+    case "endDate":
+      return new Date(query[key])
+        .toISOString()
+        .split("T")
+        .join(" ")
+        .split(".")[0];
+    default:
+      return value;
   }
 };
 
@@ -38,10 +57,9 @@ const createQuery = (pool, queries) =>
           Object.keys(query)
             .map(
               (key) =>
-                `${sqlMapping[key]} ${sqlOpMapping(
-                  Object.keys(query),
-                  key
-                )} ${pool.escape(query[key])}`
+                `${sqlMapping[key]} ${sqlOpMapping(key)} ${pool.escape(
+                  parseInput(key, query[key])
+                )}`
             )
             .join(" AND ")
         )
@@ -54,10 +72,10 @@ const getTransactions = (pool, grpcStatus, user_id, queryWhere) =>
     query = `SELECT VP1.currency AS srcCurrency, VP2.currency AS destCurrency, T.date, T.amount, T.rate FROM transaction AS T INNER JOIN virtual_portfolio AS VP1 ON T.virtual_portfolio_src_id = VP1.id INNER JOIN virtual_portfolio AS VP2 ON T.virtual_portfolio_dest_id = VP2.id WHERE (VP1.user_id = ? || VP2.user_id = ?)${
       queryWhere == "" ? ";" : ` AND (${queryWhere});`
     }`;
+    console.log(query);
     pool.query(query, [user_id, user_id], (err, results) => {
       if (err)
         return rej({ code: grpcStatus.ABORTED, message: err.sqlMessage });
-      console.log(results);
       res(results);
     });
   });
